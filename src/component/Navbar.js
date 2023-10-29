332efdd80f8035cbcfe9168463ed25d86e04385f
asafirstha <125427397+asafirstha@users.noreply.github.com>
2023-10-29 11:08:39 +0700

    Update Navbar.js

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="container">
+      <div className="navbar-left">
         <Link to="/" className="logo">
           BeritaApp
         </Link>
@@ -14,6 +15,9 @@ const Navbar = () => {
           </li>
           <li>
             <Link to="/programming">Programming</Link>
+          </li>
+          <li>
+            <Link to="/covid19">Covid-19</Link>
           </li>
           <li>
             <Link to="/saved">Saved</Link>
